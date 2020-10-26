c87e9c49bf920cdf2f59e6c1560fb8d4bd7a0120
Remitito <JRossOT@protonmail.com>
2020-10-26 17:56:50 +0800

    Updates

---
+++ precache-manifest.bf7be7fdbed255722f4b57335cf41a67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02547f33b3141c93127728fef7624c95",
+    "url": "/CV-Builder/index.html"
+  },
+  {
+    "revision": "a8c7bef7a2846af67060",
+    "url": "/CV-Builder/static/css/main.9c8950d5.chunk.css"
+  },
+  {
+    "revision": "9c0e5254bb2e6137fd48",
+    "url": "/CV-Builder/static/js/2.b315a560.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CV-Builder/static/js/2.b315a560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c7bef7a2846af67060",
+    "url": "/CV-Builder/static/js/main.7e9efce4.chunk.js"
+  },
+  {
+    "revision": "e641445fea2abcbe05d7",
+    "url": "/CV-Builder/static/js/runtime-main.06ada664.js"
+  },
+  {
+    "revision": "b13268e9b9363db2619cbb7d98730857",
+    "url": "/CV-Builder/static/media/pexels-energepiccom-288477.b13268e9.jpg"
+  }
+]);
